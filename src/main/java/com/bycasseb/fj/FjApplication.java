4078990f22e0cc479a9bb7ddff6022e23d1b375d
Felipe Soares Casseb dos Santos <s2it_fcasseb@uolinc.com>
2020-02-21 07:22:13 -0300

    Primeiro Commit

---
+++ src/main/java/com/bycasseb/fj/FjApplication.java
@@ -0,0 +1,13 @@
+package com.bycasseb.fj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bycasseb/fj/FjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bycasseb.fj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
